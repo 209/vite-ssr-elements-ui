00a56e37c1363e5da7426f8791aa21a25bab4530
ikolevatykh <kolevatykh.ilja@gmail.com>
2021-10-24 13:21:37 +0300

    elements-plus

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-dropdown>
+    <span>
+      test
+    </span>
+    <template #dropdown>
+      <div>
+        <el-dropdown-menu>
+          <el-dropdown-item>Action 1</el-dropdown-item>
+          <el-dropdown-item>Action 2</el-dropdown-item>
+          <el-dropdown-item>Action 3</el-dropdown-item>
+          <el-dropdown-item disabled>Action 4</el-dropdown-item>
+          <el-dropdown-item divided>Action 5</el-dropdown-item>
+        </el-dropdown-menu>
+      </div>
+    </template>
+  </el-dropdown>
+</template>
+<script>
+import { ElDropdown, ElDropdownMenu, ElDropdownItem } from 'element-plus';
+
+export default {
+  components: {
+    ElDropdown, ElDropdownMenu, ElDropdownItem
+  }
+};
+</script>
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import vuePlugin from '@vitejs/plugin-vue';
+
+/**
+ * @type {import('vite').UserConfig}
+ */
+export default {
+  plugins: [
+    vuePlugin(),
+  ],
+  build: {
+    minify: false
+  },
+}
